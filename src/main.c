c771d85a0c9cbce9e54a82c0dcf5976563418079
Hilmi Nugraha Wirasaputra <hilmi.nugraha@outlook.com>
2025-05-29 21:00:13 +0700

    Refactor character management functions and enhance user interaction in main menu

--- src/main.c
+++ src/main.c
@@ -18,8 +18,10 @@ data characters[100];
 
 static int characterCount = 0;
 
-void addCharacter(char name[], int level, int hp, int mp, int ap, int dp, int exp, char status[]) {
-    if (characterCount < 100) {
+void addCharacter(char name[], int level, int hp, int mp, int ap, int dp, int exp, char status[]) 
+{
+    if (characterCount < 100) 
+    {
         strcpy(characters[characterCount].name, name);
         characters[characterCount].level = level;
         characters[characterCount].hp = hp;
@@ -29,17 +31,20 @@ void addCharacter(char name[], int level, int hp, int mp, int ap, int dp, int ex
         characters[characterCount].exp = exp;
         strcpy(characters[characterCount].status, status);
         characterCount++;
-    } else {
+    } else 
+    {
         printf("Character limit reached.\n");
     }
 
 }
 
-data *searchCharacter(char name[]) {
+data *searchCharacter(char name[]) 
+{
     int low = 0;
     int high = characterCount - 1;
     int mid;
-    while (low <= high) {
+    while (low <= high) 
+    {
         mid = low + (high - low) / 2;
         int cmp = strcmp(characters[mid].name, name);
         if (cmp == 0)
@@ -58,13 +63,43 @@ data *searchCharacter(char name[]) {
     return NULL;
 }
 
-void deleteCharacter(char name[]) {
-
+void deleteCharacter(char name[]) 
+{
+    int low = 0;
+    int high = characterCount - 1;
+    int mid;
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+        int cmp = strcmp(characters[mid].name, name);
+        if (cmp == 0)
+        {
+            for (int i = mid; i < characterCount - 1; i++) 
+            {
+                characters[i] = characters[i + 1];
+            }
+            characterCount--;
+            printf("Character '%s' deleted successfully.\n", name);
+            return;
+        }
+        else if (cmp < 0)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    
+    printf("Character '%s' not found.\n", name);
 }
 
-void sortCharacterbyName(int low, int high) {
+void sortCharacterbyName(int low, int high) 
+{
     //quick sort
-    if (low < high) {
+    if (low < high) 
+    {
         data pivot = characters[low];
         int i = low;
         int j = high;
@@ -106,20 +141,29 @@ void sortCharacterbyName(int low, int high) {
     }
 }
 
-void updateCharacterStatus(char name[], char newStatus[]) {
+void updateCharacterStatus(char name[], char newStatus[]) 
+{
     data *character = searchCharacter(name);
-    if (character != NULL) {
+    if (character != NULL) 
+    {
         strcpy(character->status, newStatus);
-    } else {
+    } else 
+    {
         printf("Character not found.\n");
     }
 }
 
-void displayCharacters() {
+void displayCharacters() 
+{
     printf("Character List:\n");
-    printf("Name\tLevel\tHP\tMP\tAP\tDP\tEXP\tStatus\n");
-    for (int i = 0; i < characterCount; i++) {
-        printf("%s\t%d\t%d\t%d\t%d\t%d\t%.2f\t%s\n", 
+    printf("------------------------------------------------------------------------------\n");
+    printf("| %-15s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-8s |\n",
+
+           "Name", "Level", "HP", "MP", "AP", "DP", "EXP", "Status");
+    printf("------------------------------------------------------------------------------\n");
+    for (int i = 0; i < characterCount; i++) 
+    {
+        printf("| %-15s | %-5d | %-5d | %-5d | %-5d | %-5d | %-5.2f | %-8s |\n", 
                characters[i].name, 
                characters[i].level, 
                characters[i].hp, 
@@ -129,49 +173,130 @@ void displayCharacters() {
                characters[i].exp, 
                characters[i].status);
     }
+    printf("------------------------------------------------------------------------------\n");
+
 }
 
 int main(int argc, char const *argv[])
 {
-    
-    // addCharacter("j", 1, 100, 50, 20, 10, 0.0, "Alive");
-    // addCharacter("d", 2, 120, 60, 25, 15, 0.0, "Alive");
-    // addCharacter("e", 1, 80, 30, 15, 5, 0.0, "Alive");
-    // addCharacter("b", 3, 90, 70, 30, 20, 0.0, "Alive");
-    // addCharacter("i", 2, 110, 40, 22, 12, 0.0, "Alive");
-    // addCharacter("a", 4, 130, 80, 35, 25, 0.0, "Alive");
-    // addCharacter("h", 2, 95, 55, 18, 8, 0.0, "Alive");
-    // addCharacter("g", 1, 85, 45, 17, 7, 0.0, "Alive");
-    // addCharacter("k", 3, 105, 65, 28, 18, 0.0, "Alive");
-    // addCharacter("f", 2, 115, 75, 20, 10, 0.0, "Alive");
-    // addCharacter("c", 5, 140, 90, 40, 30, 0.0, "Alive");
     addCharacter("Hero", 1, 100, 50, 20, 10, 0.0, "Alive");
     addCharacter("Villain", 2, 120, 60, 25, 15, 0.0, "Alive");
-    addCharacter("Sidekick", 1, 80, 30, 15, 5, 0.0, "Alive");
-    addCharacter("Wizard", 3, 90, 70, 30, 20, 0.0, "Alive");
-    addCharacter("Rogue", 2, 110, 40, 22, 12, 0.0, "Alive");
-    addCharacter("Paladin", 4, 130, 80, 35, 25, 0.0, "Alive");
-    addCharacter("Archer", 2, 95, 55, 18, 8, 0.0, "Alive");
-    addCharacter("Bard", 1, 85, 45, 17, 7, 0.0, "Alive");
-    addCharacter("Monk", 3, 105, 65, 28, 18, 0.0, "Alive");
-    addCharacter("Druid", 2, 115, 75, 20, 10, 0.0, "Alive");
-    addCharacter("Necromancer", 5, 140, 90, 40, 30, 0.0, "Alive");
-    addCharacter("Knight", 3, 125, 85, 32, 22, 0.0, "Alive");
-    addCharacter("Assassin", 4, 135, 95, 38, 28, 0.0, "Alive");
-    addCharacter("Cleric", 2, 100, 70, 20, 15, 0.0, "Alive");
-    addCharacter("Sorcerer", 3, 110, 80, 30, 20, 0.0, "Alive");
-    addCharacter("Berserker", 4, 150, 100, 45, 35, 0.0, "Alive");
-    addCharacter("Ranger", 2, 105, 60, 20, 10, 0.0, "Alive");
-    addCharacter("Warlock", 3, 120, 75, 35, 25, 0.0, "Alive");
+    addCharacter("Sidekick", 1, 80, 40, 15, 5, 0.0, "Alive");
+    addCharacter("Mage", 3, 90, 70, 30, 20, 0.0, "Alive");
+    addCharacter("Warrior", 4, 150, 80, 35, 25, 0.0, "Alive");
+    addCharacter("Rogue", 2, 110, 50, 20, 10, 0.0, "Alive");
+    addCharacter("Paladin", 5, 200, 100, 40, 30, 0.0, "Alive");
+    addCharacter("Archer", 3, 130, 60, 30, 20, 0.0, "Alive");
+    addCharacter("Bard", 2, 90, 50, 20, 10, 0.0, "Alive");
+
+    while (1)
+    {
+        int choice;
+        printf("------------------------------\n");
+        printf("              MENU            \n");
+        printf("------------------------------\n");
+        printf("|%-2d| %-23s |\n", 1, "Add Character");
+        printf("|%-2d| %-23s |\n", 2, "Sort Character");
+        printf("|%-2d| %-23s |\n", 3, "Search Character");
+        printf("|%-2d| %-23s |\n", 4, "Delete Character");
+        printf("|%-2d| %-23s |\n", 5, "Update Character Status");
+        printf("|%-2d| %-23s |\n", 6, "Display Characters");
+        printf("|%-2d| %-23s |\n", 7, "Exit");
+        printf("------------------------------\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            char name[50];
+            int level, hp, mp, ap, dp;
+            float exp;
+            char status[20];
+            
+            printf("Enter character name: ");
+            scanf(" %49[^\n]", name);
+            printf("Enter character level: ");
+            scanf("%d", &level);
+            printf("Enter character HP: ");
+            scanf("%d", &hp);
+            printf("Enter character MP: ");
+            scanf("%d", &mp);
+            printf("Enter character AP: ");
+            scanf("%d", &ap);
+            printf("Enter character DP: ");
+            scanf("%d", &dp);
+            printf("Enter character EXP: ");
+            scanf("%f", &exp);
+            printf("Enter character status: ");
+            scanf("%15s", status);
+            addCharacter(name, level, hp, mp, ap, dp, exp, status);
+            printf("Character '%s' added successfully.\n", name);
+            break;
+        
+        case 2:
+            sortCharacterbyName(0, characterCount - 1);
+            printf("Characters sorted by name.\n");
+            break;
+
+        case 3:
+            char searchName[50];
+            printf("Enter character name: ");
+            scanf(" %49[^\n]", searchName);
+            sortCharacterbyName(0, characterCount - 1);
+            data *foundCharacter = searchCharacter(searchName);
+            if (foundCharacter != NULL)
+            {
+                printf("Character found\n");
+                printf("------------------------------------------------------------------------------\n");
+                printf("| %-15s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-8s |\n",
+                       "Name", "Level", "HP", "MP", "AP", "DP", "EXP", "Status");
+                printf("------------------------------------------------------------------------------\n");
+                printf("| %-15s | %-5d | %-5d | %-5d | %-5d | %-5d | %-5.2f | %-8s |\n", 
+                       foundCharacter->name, 
+                       foundCharacter->level, 
+                       foundCharacter->hp, 
+                       foundCharacter->mp, 
+                       foundCharacter->ap, 
+                       foundCharacter->dp, 
+                       foundCharacter->exp, 
+                       foundCharacter->status);
+                printf("------------------------------------------------------------------------------\n");
+            }
+            else
+            {
+                printf("Character '%s' not found.\n", searchName);
+            }
+            break;
+        case 4:
+            char deleteName[50];
+            printf("Enter character name: ");
+            scanf(" %49[^\n]", deleteName);
+            deleteCharacter(deleteName);
+            break;
+
+        case 5:
+            char updateName[50];
+            char newStatus[20];
+            printf("Enter character name: ");
+            scanf(" %49[^\n]", updateName);
+            printf("Enter new status: ");
+            scanf("%s", newStatus);
+            updateCharacterStatus(updateName, newStatus);
+            break;
+
+        case 6:
+            displayCharacters();
+            break;
+        case 7:
+            printf("Exiting the program.\n");
+            exit(0);
+            break;
+        default:
+            break;
+        }
+
+    }
     
-    data *pivot = &characters[0 + ((characterCount - 1) - 0) / 2];
-    int hasil = strcmp(characters[0].name, pivot->name);
-    printf("Comparison result: %d\n", hasil);
-    displayCharacters();
-    printf("\n");
-    printf("\n");
-    sortCharacterbyName(0, characterCount - 1);
-    displayCharacters();
     
     return 0;
 }
